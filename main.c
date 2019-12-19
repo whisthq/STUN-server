726254d37c19c4ce413b78af4a1f02c36f3e2ec5
Philippe Noel <philippe_noel@college.harvard.edu>
2019-12-19 09:22:22 -0500

    compiles

--- main.c
+++ main.c
@@ -125,10 +125,6 @@ int32_t main(int32_t argc, char **argv) {
           // get the IPv4 of the client data at this node index
           struct gll_node_t* curr_vm = gll_find_node(vm_list, j);
 
-
-
-
-
           // if the client wants to connect to this VM, we send their endpoints
           if (curr_client->data->target == curr_vm->data->ipv4) {
             // we send memory to avoid endianness byte issue
