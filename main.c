7f8d604f4a4aa4ada2e311c34d31a48ce5e4520c
philippemnoel <philippe_noel@college.harvard.edu>
2019-12-24 14:56:16 -0500

    removed some memset

--- main.c
+++ main.c
@@ -107,9 +107,9 @@ int main(void) {
       new_client.ipv4 = inet_addr(tmp); // convert to network byte order
 
       // empty memory of buffers for next IP address since this is a client
-      memset(&request_addr, 0, sizeof(request_addr));
-      memset(&recv_buff, 0, BUFLEN);
-      memset(&tmp, 0, BUFLEN);
+//      memset(&request_addr, 0, sizeof(request_addr));
+//      memset(&recv_buff, 0, BUFLEN);
+//      memset(&tmp, 0, BUFLEN);
 
       // a client also needs to send the IPv4 of the VM it wants to be paired
       // with, which it obtained through authenticating, so we receive another
